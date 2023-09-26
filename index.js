9037ee43d8e4f4b952b7025891eb4764a47c9ec5
JoseJulioJim <cdmx2115@amerike.edu.mx>
2023-09-26 09:40:34 -0600

    Base javascript

---
+++ index.js
@@ -0,0 +1,58 @@
+let nombre = "José Julio Jiménez del Valle",
+edad = 20,
+telefono = 9211793052,
+correo = jimenezdelvallejosejuliogmail.com;
+
+let mensajeConetando =
+"Soy " +
+nombre +
+"mi edad es " +
+edad +
+" años, y mi celular y correo son " +
+telefono +
+" y " +
+correo;
+
+console.log(mensajeConetando)
+
+let pasatiempos = "Jugar Videojuegos, Escuchar Musica y ver anime"
+console.log (pasatiempos)
+
+let Pelicula1 = {
+    nombre: "Las Locuras del Emperador",
+    añoDeEstreno: 2000,
+    disponibleEnStreaming: true,
+    castPrincipal: ["David Spade", "John Goodman", "Eartha Kitt"],
+    detallesProduccion: {
+        productora: "Disney",
+        director: "Mark Dindal",
+    },
+};
+
+console.log(Pelicula1)
+
+let Pelicula2 = {
+    nombre: "Spiderman Across the Spiderverse",
+    añoDeEstreno: 2023,
+    disponibleEnStreaming: true,
+    castPrincipal: ["Shameik Moore", "Hailee Steinfeld", "Oscar Isaac"],
+    detallesProduccion: {
+        productora: "Sony Pictures",
+        director: "Joaquim Dos Santos",
+    },
+};
+
+console.log(Pelicula2)
+
+let Pelicula3 = {
+    nombre: "Parasite",
+    añoDeEstreno: 2019,
+    disponibleEnStreaming: true,
+    castPrincipal: ["Song Kang-ho", "Choi Woo-shik", "Cho Yeo-jeong"],
+    detallesProduccion: {
+        productora: "CJ Entertainmet",
+        director: "Bong Joon-ho",
+    },
+};
+
+console.log(Pelicula3)
